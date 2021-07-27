9ccb9c6d23993c17c2f76e5fd1fa71eb91fe0bb7
saron118965 <delinafiyona@gmail.com>
2021-07-27 07:01:04 -0700

    work 1 is done

--- src/day1/Second.java
+++ src/day1/Second.java
@@ -1,9 +1,9 @@
 package day1;
 
 public class Second {
+    public static void main(String[] args) {
+
+    }
     // work 1 is done here
-    // work 2 is done here
-    // work 3 is done here
-    // work 4 is done here
-    // work 5 is done here
+
 }
