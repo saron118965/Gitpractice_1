ac97f8d582ff2c18fa6cd42af2f3eca12a7ccfbf
saron118965 <delinafiyona@gmail.com>
2021-07-30 05:47:04 -0700

    added few items

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -5,5 +5,6 @@ import java.util.ArrayList;
 public class ListPractice {
     public static void main(String[] args) {
         List<String> lst = new ArrayList<>();
+        // adding one item
     }
 }
