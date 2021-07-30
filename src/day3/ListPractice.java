85d2670679cd638505f5bd7b0a1eda93abb67e79
saron118965 <delinafiyona@gmail.com>
2021-07-30 05:47:48 -0700

    printing

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public class ListPractice {
     public static void main(String[] args) {
-        List<String> lst = new ArrayList<>();
+       // List<String> lst = new ArrayList<>();
         // adding one item
+        //printing out
     }
 }
